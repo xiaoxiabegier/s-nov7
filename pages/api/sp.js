e918a3ff272f8b7db12acbccbd891ce4e4263ae8
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-11-06 23:37:34 -0800

    intiial

--- pages/api/sp.js
+++ pages/api/sp.js
@@ -12,6 +12,6 @@ export default async function handler(req,res){
         returnArray.push(doc.data())
     });
 
-    res.status(200).json(returnArray)
+    res.status(200).json(returnArray[0])
 
 }

8e3a8dd8e4627c63a7955d0e7778c780aa9be68e
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-11-06 21:44:24 -0800

    intial

---
+++ pages/api/dynamicPaymentPaths.js
@@ -0,0 +1,18 @@
+import { collection, query, where, getDocs } from "firebase/firestore";
+import {db} from "../../public/firebase";
+
+export default async function handler(req, res) {
+
+    const {propertyid} = req.headers
+
+    const unpaidCol = query(collection(db, "payments"), where("status", "==", "unpaid"), where("propertyID", "==", propertyid))
+
+    const paths = await getDocs(unpaidCol).then( snapshot => {
+        let returnArray = []
+        snapshot.forEach(currentItem => returnArray.push({params: {href: currentItem.get("href"), propertyID: currentItem.get("propertyID")}}));
+        return returnArray
+ })
+    res.status(200).json(paths)
+
+
+}
